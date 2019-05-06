8fc3f88fa9ae239c44ecfee410ca55d005a0d8f2
cactiw <knyazev1514@gmail.com>
2019-05-07 00:01:10 +0300

    Сделал синхронизацию состава мида между двумя ботами

--- order_files/bin/castle_update_monitor.py
+++ order_files/bin/castle_update_monitor.py
@@ -5,13 +5,18 @@
 from globals import update_request_queue
 from order_files.bin.order import recashe_order_chats
 from order_files.work_materials.globals import conn
+from castle_files.bin.castle import fill_mid_players
 
 
 # Сам монитор, работает до получения None из очереди update_request_queue
 def castle_update_monitor():
     cursor = conn.cursor()
-    guild_id = update_request_queue.get()
-    while guild_id is not None:
-        recashe_order_chats(new_cursor=cursor)  # Неоптимально, но это не должно вызываться часто.
-        # upd: TODO: говнище, всё переделать. Могут не уйти приказы из-за этого
-        guild_id = update_request_queue.get()
+    data = update_request_queue.get()
+    while data is not None:
+        if data[0] == "update_guild":
+            recashe_order_chats(new_cursor=cursor)  # Неоптимально, но это не должно вызываться часто.
+            # upd: TODO: говнище, всё переделать. Могут не уйти приказы из-за этого
+        elif data[0] == "update_mid":
+            fill_mid_players(other_process=True)
+
+        data = update_request_queue.get()
