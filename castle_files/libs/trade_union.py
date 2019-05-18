b4f8f01467c8a731fa7a3e282f545618b595ac10
cactiw <knyazev1514@gmail.com>
2019-05-18 22:14:41 +0300

    Фиксы с профсоюзами

--- castle_files/libs/trade_union.py
+++ castle_files/libs/trade_union.py
@@ -3,16 +3,17 @@ from castle_files.work_materials.globals import cursor
 
 class TradeUnion:
 
-    def __init__(self, id, creator_id, name, players, view_link):
+    def __init__(self, id, creator_id, name, players, view_link, chat_id):
         self.id = id
         self.creator_id = creator_id
         self.name = name
         self.players = players
         self.view_link = view_link
+        self.chat_id = chat_id
 
     @staticmethod
     def get_union(union_id=None, creator_id=None, chat_id=None):
-        request = "select id, creator_id, name, players, view_link from trade_unions where "
+        request = "select id, creator_id, name, players, view_link, chat_id from trade_unions where "
         arg = None
         if union_id is not None:
             request += "id = %s"
@@ -27,10 +28,10 @@ class TradeUnion:
         row = cursor.fetchone()
         if row is None:
             return None
-        return TradeUnion(row[0], row[1], row[2], row[3], row[4])
+        return TradeUnion(row[0], row[1], row[2], row[3], row[4], row[5])
 
     def update_to_database(self):
         request = "update trade_unions set creator_id = %s, name = %s, players = %s, view_link = %s, chat_id = %s" \
                   " where id = %s"
-        cursor.execute(request, (self.creator_id, self.name, self.players, self.view_link, self.id,))
+        cursor.execute(request, (self.creator_id, self.name, self.players, self.view_link, self.chat_id, self.id,))
         return 0
