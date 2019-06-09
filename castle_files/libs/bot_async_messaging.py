81c3cace87d6ee33b6a6baf02754226be1fc2e10
cactiw <knyazev1514@gmail.com>
2019-06-09 20:44:03 +0300

    Фикс "забивания" воркеров при спаме в одном чате

--- castle_files/libs/bot_async_messaging.py
+++ castle_files/libs/bot_async_messaging.py
@@ -10,6 +10,7 @@ import traceback
 
 MESSAGE_PER_SECOND_LIMIT = 29
 MESSAGE_PER_CHAT_LIMIT = 3
+MESSAGE_PER_CHAT_MINUTE_LIMIT = 20
 
 UNAUTHORIZED_ERROR_CODE = 2
 BADREQUEST_ERROR_CODE = 3
@@ -19,15 +20,18 @@ MAX_MESSAGE_LENGTH = 4096
 
 class AsyncBot(Bot):
 
-    def __init__(self, token, workers = 4, request_kwargs = None):
+    def __init__(self, token, workers=4, request_kwargs=None):
         counter_rlock = threading.RLock()
         self.counter_lock = threading.Condition(counter_rlock)
         self.message_queue = multiprocessing.Queue()
+        self.waiting_chats_message_queue = multiprocessing.Queue()
         self.processing = True
         self.num_workers = workers
         self.messages_per_second = 0
         self.messages_per_chat = {}
+        self.messages_per_chat_per_minute = {}
         self.workers = []
+        self.resending_workers = []
         if request_kwargs is None:
             request_kwargs = {}
         con_pool_size = workers + 4
@@ -52,13 +56,25 @@ class AsyncBot(Bot):
             while True:
                 lock.acquire()
                 messages_per_current_chat = self.messages_per_chat.get(chat_id)
+                messages_per_current_chat_per_minute = self.messages_per_chat_per_minute.get(chat_id)
                 if messages_per_current_chat is None:
                     messages_per_current_chat = 0
-                if self.messages_per_second < MESSAGE_PER_SECOND_LIMIT and messages_per_current_chat < MESSAGE_PER_CHAT_LIMIT:
+                if messages_per_current_chat_per_minute is None:
+                    messages_per_current_chat_per_minute = 0
+                if self.messages_per_second < MESSAGE_PER_SECOND_LIMIT and messages_per_current_chat < \
+                        MESSAGE_PER_CHAT_LIMIT and messages_per_current_chat_per_minute < MESSAGE_PER_CHAT_MINUTE_LIMIT:
                     self.messages_per_second += 1
-                    self.messages_per_chat.update({chat_id : messages_per_current_chat + 1})
+                    self.messages_per_chat.update({chat_id: messages_per_current_chat + 1})
+                    self.messages_per_chat_per_minute.update({chat_id: messages_per_current_chat_per_minute + 1})
                     lock.release()
                     break
+                else:
+                    if self.messages_per_second < MESSAGE_PER_SECOND_LIMIT:
+                        # Сообщения в эту секунду ещё можно отправлять
+                        # Кладём в другую очередь
+                        self.waiting_chats_message_queue.put(MessageInQueue(*args, **kwargs))
+                        lock.release()
+                        return None
                 lock.release()
                 lock.wait()
         finally:
@@ -72,13 +88,25 @@ class AsyncBot(Bot):
         except Unauthorized:
             release = threading.Timer(interval=1, function=self.__releasing_resourse, args=[chat_id])
             release.start()
+            release = threading.Timer(interval=60, function=self.__releasing_minute_resourse, args=[chat_id])
+            release.start()
             return UNAUTHORIZED_ERROR_CODE
         except BadRequest:
             logging.error(traceback.format_exc())
             release = threading.Timer(interval=1, function=self.__releasing_resourse, args=[chat_id])
             release.start()
+            release = threading.Timer(interval=60, function=self.__releasing_minute_resourse, args=[chat_id])
+            release.start()
             return BADREQUEST_ERROR_CODE
         except (TimedOut, NetworkError):
+            release = threading.Timer(interval=1, function=self.__releasing_resourse, args=[chat_id])
+            release.start()
+            release = threading.Timer(interval=60, function=self.__releasing_minute_resourse, args=[chat_id])
+            release.start()
+            logging.error(traceback.format_exc())
+            return None
+
+            # Временно отключена повторная попытка отправить
             retry = kwargs.get('retry')
             if retry is None:
                 retry = 0
@@ -91,13 +119,18 @@ class AsyncBot(Bot):
             message = super(AsyncBot, self).send_message(*args, **kwargs)
         release = threading.Timer(interval=1, function=self.__releasing_resourse, args=[chat_id])
         release.start()
+        release = threading.Timer(interval=60, function=self.__releasing_minute_resourse, args=[chat_id])
+        release.start()
         return message
 
     def start(self):
         for i in range(0, self.num_workers):
-            worker = threading.Thread(target = self.__work, args = ())
+            worker = threading.Thread(target=self.__work, args=())
             worker.start()
             self.workers.append(worker)
+            resending_worker = threading.Thread(target=self.__resend_work, args=())
+            resending_worker.start()
+            self.resending_workers.append(worker)
 
     def stop(self):
         self.processing = False
@@ -130,7 +163,21 @@ class AsyncBot(Bot):
                 self.counter_lock.notify_all()
                 return
             mes_per_chat -= 1
-            self.messages_per_chat.update({chat_id : mes_per_chat})
+            self.messages_per_chat.update({chat_id: mes_per_chat})
+            self.counter_lock.notify_all()
+
+    def __releasing_minute_resourse(self, chat_id):
+        with self.counter_lock:
+            mes_per_chat = self.messages_per_chat_per_minute.get(chat_id)
+            if mes_per_chat is None:
+                self.counter_lock.notify_all()
+                return
+            if mes_per_chat == 1:
+                self.messages_per_chat_per_minute.pop(chat_id)
+                self.counter_lock.notify_all()
+                return
+            mes_per_chat -= 1
+            self.messages_per_chat_per_minute.update({chat_id: mes_per_chat})
             self.counter_lock.notify_all()
 
     def __work(self):
@@ -144,8 +191,21 @@ class AsyncBot(Bot):
                 return 0
         return 0
 
+    def __resend_work(self):
+        message_in_queue = self.waiting_chats_message_queue.get()
+        while self.processing and message_in_queue:
+            args = message_in_queue.args
+            kwargs = message_in_queue.kwargs
+            mes = self.actually_send_message(*args, **kwargs)
+            if mes is None:
+                time.sleep(0.1)
+            message_in_queue = self.waiting_chats_message_queue.get()
+            if message_in_queue is None:
+                return 0
+        return 0
 
-class MessageInQueue():
+
+class MessageInQueue:
 
     def __init__(self, *args, **kwargs):
         self.args = args
