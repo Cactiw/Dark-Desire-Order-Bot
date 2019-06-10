934041221f925e928b17bca31f4f64d515b1eeb1
cactiw <knyazev1514@gmail.com>
2019-06-10 12:12:27 +0300

    Дал хуиз Кизе

--- castle_files/bin/profile.py
+++ castle_files/bin/profile.py
@@ -168,7 +168,7 @@ def profile(bot, update, user_data=None):
                      disable_web_page_preview=True)
 
 
-trade_divisions_access_list = [439637823, 320365073, 334443202]
+trade_divisions_access_list = [439637823, 320365073, 334443202, 407981701]
 # Игроки, которым дал доступ к хуизу в связи с альянсами
 
 
