32f6837a0890227506998b6a1183b4568a24b04c
cactiw <knyazev1514@gmail.com>
2019-07-30 18:04:01 +0300

    Мобы работают только с форвардами из чв

--- castle_files/work_materials/filters/mob_filters.py
+++ castle_files/work_materials/filters/mob_filters.py
@@ -5,7 +5,7 @@ from castle_files.work_materials.filters.general_filters import filter_is_pm, fi
 
 class FilterMobMessage(BaseFilter):
     def filter(self, message):
-        return (filter_is_chat_wars_forward(message) or True) and filter_is_pm(message) and \
+        return filter_is_chat_wars_forward(message) and filter_is_pm(message) and \
                '/fight' in message.text.lower()
 
 
