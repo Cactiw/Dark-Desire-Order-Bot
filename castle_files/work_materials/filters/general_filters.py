6f3824bb5f3d23f189cae46d697bec18445f8581
cactiw <knyazev1514@gmail.com>
2019-08-30 14:35:27 +0300

    v12 adapt

--- castle_files/work_materials/filters/general_filters.py
+++ castle_files/work_materials/filters/general_filters.py
@@ -6,9 +6,13 @@ from telegram.ext import BaseFilter
 from castle_files.work_materials.globals import CHAT_WARS_ID, MERC_ID
 from castle_files.bin.service_functions import check_access
 
+from telegram import Update
+
 
 class FilterIsChatWarsForward(BaseFilter):
     def filter(self, message):
+        if isinstance(message, Update):
+            message = message.message
         return message.forward_from is not None and message.forward_from.id == CHAT_WARS_ID
 
 
@@ -18,6 +22,8 @@ filter_is_chat_wars_forward.update_filter = True
 
 class FilterIsPM(BaseFilter):
     def filter(self, message):
+        if isinstance(message, Update):
+            message = message.message
         if message.from_user is None:
             return False
         return message.chat_id == message.from_user.id
@@ -29,6 +35,8 @@ filter_is_pm.update_filter = True
 
 class FilterHasAccess(BaseFilter):
     def filter(self, message):
+        if isinstance(message, Update):
+            message = message.message
         return check_access(message.from_user.id)
 
 
@@ -38,6 +46,8 @@ filter_has_access.update_filter = True
 
 class FilterIsMerc(BaseFilter):
     def filter(self, message):
+        if isinstance(message, Update):
+            message = message.message
         return message.from_user.id == MERC_ID
 
 
