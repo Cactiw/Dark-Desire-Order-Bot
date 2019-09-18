90d83b69d1766302abfd9ccd5b8dc82b97fb4a88
cactiw <knyazev1514@gmail.com>
2019-09-18 21:54:46 +0300

    Обновил версию телетона

--- castle_files/bin/telethon_script.py
+++ castle_files/bin/telethon_script.py
@@ -1,5 +1,5 @@
-from telethon import TelegramClient, events
-from telethon.utils import PeerChannel
+from telethon.sync import TelegramClient, events
+from telethon.tl.types import PeerChannel
 
 from castle_files.work_materials.globals import RESULTS_PARSE_CHANNEL_ID
 
@@ -16,23 +16,35 @@ castles_stats_queue = Queue()
 
 TEST_CHANNEL_ID = 1353017829
 
+guilds_str = ""
+
 
 def script_work():
     global client
-    admin_client = TelegramClient(username, api_id, api_hash, update_workers=1, spawn_read_thread=False)
-    admin_client.start(phone, password)
+    admin_client = TelegramClient(username, api_id, api_hash)
+    # admin_client.start(phone, password)
+    #
+    # client = admin_client
+    with admin_client as client:
+        admin_client.get_entity("ChatWarsBot")
+        client.add_event_handler(stats_handler, event=events.NewMessage)
+        print("telegram script launched")
 
-    client = admin_client
-    admin_client.get_entity("ChatWarsBot")
-    client.add_event_handler(stats_handler, event=events.NewMessage)
-    print("telegram script launched")
-
-    admin_client.idle()
+        admin_client.run_until_disconnected()
 
 
-def stats_handler(event):
+async def stats_handler(event):
+    global guilds_str
     text = event.message.message
-    if event.message.to_id == PeerChannel(RESULTS_PARSE_CHANNEL_ID) and 'Результаты сражений:' in text:
-        print("put stats in queue")
-        castles_stats_queue.put(text)
-        return
+    if event.message.to_id == PeerChannel(RESULTS_PARSE_CHANNEL_ID) and \
+            ('Результаты сражений:' in text or '⛺️Гильдия' in text or '⛺Гильдия' in text):
+        logging.error("Received data from telegram, sending: {}".format(text))
+        if '⛺️Гильдия' in text:
+            guilds_str += text + "\n"
+            logging.info("Adding text to guilds_str = {}".format(guilds_str))
+        else:
+            print("put stats in queue")
+            castles_stats_queue.put(text)
+            castles_stats_queue.put(guilds_str)
+            guilds_str = ""
+            return
