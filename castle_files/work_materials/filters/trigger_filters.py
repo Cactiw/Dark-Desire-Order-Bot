25a476d1953e9fcbd808c41ce35ceb34884585a5
Cactiw <Knyazev1514@gmail.com>
2020-01-14 13:41:08 +0300

    Global triggers in PM final fix

--- castle_files/work_materials/filters/trigger_filters.py
+++ castle_files/work_materials/filters/trigger_filters.py
@@ -6,8 +6,8 @@ from castle_files.work_materials.filters.general_filters import filter_is_pm
 class FilterIsTrigger(BaseFilter):
     def filter(self, message):
         local_triggers = triggers_in.get(message.chat_id)
-        return not filter_is_pm and ((local_triggers is not None and message.text.lower() in local_triggers) or
-                                     message.text.lower() in global_triggers_in)
+        return not filter_is_pm(message) and ((local_triggers is not None and message.text.lower() in local_triggers) or
+                                              message.text.lower() in global_triggers_in)
 
 
 filter_is_trigger = FilterIsTrigger()
