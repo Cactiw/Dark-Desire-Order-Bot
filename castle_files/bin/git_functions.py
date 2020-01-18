0d32aad23f30a4497b0c4ca2bf64f71595118f59
Cactiw <Knyazev1514@gmail.com>
2020-01-18 17:24:55 +0300

    Fixed git commit message texts

--- castle_files/bin/git_functions.py
+++ castle_files/bin/git_functions.py
@@ -11,12 +11,13 @@ import datetime
 
 def last_commits(bot, update):
     repo = git.Repo(os.getcwd())
-    master = repo.head.reference
-    commits = master.log()[-1:-6:-1]
+    # master = repo.head.reference
+    # repo.git.
+    commits = list(repo.iter_commits('master', max_count=7))
     response = "Последние коммиты в гите:\n"
     for line in commits:
         # parse = re.match("\\S+ \\S+ (.+) \\S+ (\\d+) \\S+	(.*)", line)
-        response += "<code>{} ({})</code>:\n{}\n\n".format(
-            line.actor.name, datetime.datetime.fromtimestamp(int(line.time[0])).strftime('%Y-%m-%d %H:%M:%S'),
-            line.message.partition(": ")[2])
+        response += "<code>{} ({})</code>:\n{}\n".format(
+            line.author.name, line.committed_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+            line.message)
     bot.send_message(chat_id=update.message.from_user.id, text=response, parse_mode='HTML')
