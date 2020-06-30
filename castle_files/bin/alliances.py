a7ac0abee52eecb9596a336998c5df5694d4265b
Cactiw <40821969+Cactiw@users.noreply.github.com>
2020-06-30 16:09:35 +0300

    Allowed alliances for Brown Author

--- castle_files/bin/alliances.py
+++ castle_files/bin/alliances.py
@@ -21,7 +21,7 @@ import datetime
 from telegram import InlineKeyboardMarkup
 from functools import reduce, wraps
 
-ALLOWED_LIST = ['Creepy Balboa', 'Enchanted Warrior', 'Coarse Mercury']
+ALLOWED_LIST = ['Creepy Balboa', 'Enchanted Warrior', 'Coarse Mercury', 'Brown Author']
 
 
 def alliance_access(func):
