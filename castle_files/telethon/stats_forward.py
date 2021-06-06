1444316a580b50fb3bc459fbde61d220a6de8d74
Cactiw <Knyazev1514@gmail.com>
2021-06-06 22:59:30 +0300

    Added forward_script

---
+++ castle_files/telethon/stats_forward.py
@@ -0,0 +1,19 @@
+from telethon import TelegramClient, events
+import asyncio
+
+from config import api_id, api_hash
+
+
+async def main():
+
+    session = "./current.session"
+    async with TelegramClient(session, api_id, api_hash) as client:
+
+        @client.on(events.NewMessage(chats=(-1001369273162,)))
+        async def handler(event: events.NewMessage.Event):
+            await client.send_message(-1001274086574, event.message.message)
+
+        await client.run_until_disconnected()
+
+
+asyncio.get_event_loop().run_until_complete(main())
