b4ffc622e947cc1fba62b5c2275c2187c607f068
cactiw <Knyazev1514@gmail.com>
2022-09-20 10:35:59 +0300

    Change time to battle

--- castle_files/bin/service_functions.py
+++ castle_files/bin/service_functions.py
@@ -177,10 +177,9 @@ def get_message_and_player_id(update):
 def get_time_remaining_to_battle():
     now = get_current_datetime() - datetime.datetime.combine(
         datetime.datetime.now().date(), datetime.time(hour=0))
-    if now < datetime.timedelta(hours=1):
-
-        return datetime.timedelta(hours=1) - now
-    time_from_first_battle = now - datetime.timedelta(hours=1)
+    if now < datetime.timedelta(hours=2):
+        return datetime.timedelta(hours=2) - now
+    time_from_first_battle = now - datetime.timedelta(hours=2)
     while time_from_first_battle > datetime.timedelta(hours=8):
         time_from_first_battle -= datetime.timedelta(hours=8)
     time_remaining = datetime.timedelta(hours=8) - time_from_first_battle
